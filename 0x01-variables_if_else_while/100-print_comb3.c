22209b940b1b9c36fc60c7f6b640a8e53a11a25b
surprise <surprisemalatji2@gmail.com>
2022-09-11 15:10:34 -0700

    TASK 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - all possible different combinations of 2 digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int i_d;
+
+	while (c <= 99)
+	{
+		f_d = (c / 10 + '0');
+		i_d = (c % 10 + '0');
+		if (f_d < i_d)
+		{
+			putchar(f_d);
+			putchar(i_d);
+		if (c != 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+putchar('\n');
+return (0);
+}
