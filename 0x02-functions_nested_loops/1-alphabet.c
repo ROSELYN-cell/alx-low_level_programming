876d587bb0642db924ffbab9e5727df035eaa7ea
surprise <surprisemalatji2@gmail.com>
2022-09-13 13:32:53 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print a to z in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+}
