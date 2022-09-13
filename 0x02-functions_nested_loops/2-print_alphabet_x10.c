417f7ba543518ecfc87392a2ce04a959c3128107
surprise <surprisemalatji2@gmail.com>
2022-09-13 13:48:47 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print a to z 1o times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; <= 9; i++)
+
+	{
+		for (c = 'a'; c = 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
