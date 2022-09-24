03fadd25be1bc60fba798d9fd57e61c5241c775c
surprise <surprisemalatji2@gmail.com>
2022-09-24 01:55:22 -0700

    task 3

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -18,3 +18,30 @@ double _sqrt(double s)
 		}
 		return (sqrt);
 }
+
+/**
+ * largest_prime_factor - finds and prints the
+ * largest prime factor
+ * @num: number to find
+ */
+void largest_prime_factor(long int num)
+{
+	int prmNu, largest;
+
+	/*first divide with the smallest prime number (two) */
+	while (num % 2 == 0)
+		num = num / 2;
+
+	/*num must be odd , proceed to nect prime number(plus two)*/
+	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
+	{
+		while (num % prmNu == 0)
+		{
+			num = num / prmNu;
+			largest = prmNu;
+		}
+	}
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
+}
