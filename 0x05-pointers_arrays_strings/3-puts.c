448dafef5d7ea7f10ab0b640c1d886e18cc1fc29
surprise <surprisemalatji2@gmail.com>
2022-09-26 13:13:07 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string
+ * Retrun: no error
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(5);
+}
